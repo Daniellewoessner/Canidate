c2318d40172449e0112749e9af52fe7d9f11ceb2
Daniellewoessner <dlegare7792@gmail.com>
2025-01-28 23:56:59 -0500

    push to main branch

--- src/components/server.ts
+++ src/components/server.ts
@@ -1,16 +1,17 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename); 
+const __dirname = dirname(__filename);
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-app.use(express.static(join(__dirname, '../../dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('*', (_req: Request, res: Response) => {
-  res.sendFile(join(__dirname, '../../dist', 'index.html'));
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
