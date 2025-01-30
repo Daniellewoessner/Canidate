93a21e596c92e34b39c65c573884a22284001ed6
Daniellewoessner <dlegare7792@gmail.com>
2025-01-29 19:24:10 -0500

    push to main

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './environment',
-  base:'/Canidate/',
   plugins: [react()],
+  base: 'https://github.com/Daniellewoessner/Canidate.git',  
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].ts',
+        entryFileNames: 'assets/[name].[hash].ts',
+      }
+    },
+    assetsInlineLimit: 4096,
+    sourcemap: true,
+    emptyOutDir: true
+  },
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/src/assets',
+      '@components': '/src/components'
+    }
+  }
 });
